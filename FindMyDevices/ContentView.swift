a9658cf07f69c096323c9fc32aa272452d42854e
Airy André <airy.andre@free.fr>
2024-02-07 15:47:03 +0100

    Initial Commit

---
+++ FindMyDevices/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindMyDevices
+//
+//  Created by Airy ANDRE on 07/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindMyDevices/FindMyDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyDevicesApp.swift
+//  FindMyDevices
+//
+//  Created by Airy ANDRE on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FindMyDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
